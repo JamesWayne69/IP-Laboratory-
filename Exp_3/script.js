ff37235a96c12ef203a7d0622df99aced46f4dec
JamesWayne69 <90917945+JamesWayne69@users.noreply.github.com>
2023-02-17 15:52:11 +0530

    one condition done

--- Exp_3/script.js
+++ Exp_3/script.js
@@ -1,9 +1,8 @@
 function validate() {
     var flag = 1;
-    if(document.forms["myform"]["pname"].value == "" && document.forms["myform"]["c_no"].value == "" && document.forms["myform"]["c_name"].value == "" && document.forms["myform"]["g_origin"].value == "" && document.forms["myform"]["age"].value == "" && document.forms["myform"]["sex"].value == "" && document.forms["myform"]["doa"].value == "" && document.forms["myform"]["studio"].value == "" && document.forms["myform"]["pub"].value == "" && document.forms["myform"]["desc"].value == "") {
+    if(document.forms["myform"]["p_name"].value == "" && document.forms["myform"]["c_no"].value == "" && document.forms["myform"]["c_name"].value == "" && document.forms["myform"]["g_origin"].value == "" && document.forms["myform"]["age"].value == "" && document.forms["myform"]["sex"].value == "" && document.forms["myform"]["doa"].value == "" && document.forms["myform"]["studio"].value == "" && document.forms["myform"]["pub"].value == "" && document.forms["myform"]["desc"].value == "") {
         flag = 0;
     }
-    alert("you filled the list");
 
     if(flag == 0) {
         alert("Check'yer data mate");
