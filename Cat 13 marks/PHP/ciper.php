73c833795c96d32fcf314924d34d63e6c0def40e
JamesWayne69 <90917945+JamesWayne69@users.noreply.github.com>
2023-04-13 14:18:20 +0530

    done

--- Cat 13 marks/PHP/ciper.php
+++ Cat 13 marks/PHP/ciper.php
@@ -4,16 +4,16 @@
     </head>
     <body>
         <?php 
-            $plaintext = $_GET("text");
-            $key = intval($_GET("key"));
-            //$plaintext = "ABCD";
-            //$key = 3;
+            //$plaintext = $_GET("text");
+            //$key = intval($_GET("key"));
+            $plaintext = "ABCD";
+            $key = 3;
             $cipher = "";
             for($i=0;$i<strlen($plaintext);$i++) {
                 $c = $plaintext[$i];
                 if(ctype_lower($c))
                     echo chr((ord($plaintext[$i])-$key-ord("a")+26)%26+ord("a"));
-                else if(ctype_alpha($c))
+                else if(ctype_upper($c))
                     echo chr((ord($plaintext[$i])-$key-ord("A")+26)%26+ord("A"));
                 else
                     echo $plaintext[$i];
